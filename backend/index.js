6c8b3538b168b88f5177e6bc7421afc194666f06
payalkherajani <payal.kherajani19@gmail.com>
2021-08-02 13:06:38 +0530

    integration-1st-attempt-done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const { v4: uuidv4 } = require('uuid');
 const app = express()
 const connectDB = require('./config/db')
 const dotenv = require('dotenv')
 dotenv.config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 app.use(cors())
 connectDB()
@@ -11,6 +13,24 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("Hello World")
+})
+
+app.post('/payment', (req, res) => {
+    const { product, token } = req.body;
+    console.log("PRODUCT", product);
+    const idempontencyKey = uuidv4()
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: product.name
+        }, { idempontencyKey })
+    }).then((result) => res.status(200).json(result)).catch((err) => console.log(err))
 })
 
 const PORT = process.env.PORT || 5000
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
 
 function App() {
+  console.log(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
+  const [product, setProduct] = useState({
+    name: 'Basic Plan',
+    price: 1
+  })
+
+  const makePayment = (token) => {
+    const body = {
+      token,
+      product
+    }
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+
+    return fetch(`http://localhost:5000/payment`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body)
+    }).then((res) => console.log({ res })).catch((err) => console.log(err))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learning stripe
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StripeCheckout
+          stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
+          token={makePayment}
+          name={`Buy Plan at $ ${product.price}`}
+          amount={product.price * 100}
+        />
       </header>
     </div>
   );
